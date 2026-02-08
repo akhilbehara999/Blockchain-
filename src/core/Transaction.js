9420115128ae8524676b6b047e06e0242c1bc0f7
Akhil Behara <akhilbehara97@gmail.com>
2026-02-08 07:24:15 +0530

    Delete src directory

--- src/core/Transaction.js
+++
@@ -1,9 +0,0 @@
-export class Transaction {
-    constructor(sender, receiver, amount) {
-        this.id = Date.now().toString() + Math.random().toString(36).substr(2, 9);
-        this.sender = sender;
-        this.receiver = receiver;
-        this.amount = parseFloat(amount);
-        this.timestamp = new Date().toISOString();
-    }
-}
